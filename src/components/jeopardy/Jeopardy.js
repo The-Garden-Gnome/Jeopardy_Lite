6eef8e43010d2484f443e2ebc78705cebd597774
Samantha Taylor <staylor@mort.is>
2020-08-24 16:44:27 -0400

    Changed setup. Instead of doing a separate component for the form, I combined it inside jeopardy. Added the functionality to the form. It now checks for the answers and will add/subtract score.

--- src/components/jeopardy/Jeopardy.js
+++ src/components/jeopardy/Jeopardy.js
@@ -1,50 +1,143 @@
 import React, { Component } from 'react';
 //import our service
 import JeopardyService from "../../jeopardyService";
-import JeopardyAnswer from '../jeopardy_answer/JeopardyAnswer';
-class Jeopardy extends Component {
-  //set our initial state and set up our service as this.client on this component
-  constructor(props){
-    super(props);
-    this.client = new JeopardyService();
-    this.state = {
-      data: {},
-      score: 0
-    }
-  }
-  //get a new random question from the API and add it to the data object in state
-  getNewQuestion() {
-    return this.client.getQuestion().then(result => {
-      this.setState({
-        data: result.data[0]
-      })
-    })
-  }
-  //when the component mounts, get a the first question
-  componentDidMount() {
-    this.getNewQuestion();
-  }
-  //display the results on the screen
-  render() {
 
-    let category = "loading..."
-if (this.state.data.category){
-    category = this.state.data.category.title
+class Jeopardy extends Component {
+    //set our initial state and set up our service as this.client on this component
+    constructor(props) {
+        super(props);
+        this.client = new JeopardyService();
+        this.state = {
+            data: {},
+            score: 0,
+            submitted: false,
+            formData: {
+                answer: '',
+            }
+        }
+    }
+    //get a new random question from the API and add it to the data object in state
+    getNewQuestion() {
+        return this.client.getQuestion().then(result => {
+            this.setState({
+                data: result.data[0]
+            })
+        })
+    }
+    //when the component mounts, get a the first question
+    componentDidMount() {
+        this.getNewQuestion();
+    }
+    //display the results on the screen
+
+
+    handleChange = (event) => {
+        const formData = { ...this.state.formData };
+        formData[event.target.answer] = event.target.value;
+        
+
+        this.setState({
+            formData: {
+                answer: event.target.value
+            }
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            submitted: true
+        })
+
+        if (this.state.formData.answer === this.state.data.answer) {
+            const scoreData = { ...this.state };
+        scoreData[event.target.score] = this.state.data.value;
+            this.setState({
+                score: this.state.score + this.state.data.value
+            })
+    }
+
+    else{
+        const scoreData = { ...this.state };
+        scoreData[event.target.score] = this.state.data.value;
+            this.setState({
+                score: this.state.score - this.state.data.value
+            })
+    }
 }
 
-    return (
+    resetForm = (event) => {
+        this.getNewQuestion()
+        this.setState({
+            submitted: false,
+            formData: {
+                answer: '',
+            },
+        })
 
+    }
 
+    render() {
 
-      <div> <strong>User Score: </strong> {this.state.score} < br/>
-          <strong>Category: </strong> {category}<br />
-          <strong>Question: </strong> {this.state.data.question} < br/>
-          <strong>Value: </strong> {this.state.data.value}< br/>
-         
-         <JeopardyAnswer />
-        
-      </div>
-    );
-  }
+        let category = "loading..."
+        if (this.state.data.category) {
+            category = this.state.data.category.title
+        }
+    
+        if(this.state.submitted && this.state.formData.answer === this.state.data.answer){
+            return (
+                <div>
+                <div><h4>You answered correct! Answer is: {this.state.data.answer}</h4><br /></div>
+                <div> <strong>User Score: </strong> {this.state.score} < br />
+                </div>
+                <div className="resetForm">
+                    <button onClick={this.resetForm}>Next Question</button>
+                </div>
+                </div>
+            )
+        }
+
+        if(this.state.submitted && this.state.formData.answer !== this.state.data.answer){
+            return (
+                <div>
+                <div><h4>You answered Incorrect! Answer is: {this.state.data.answer}</h4><br /></div>
+                <div> <strong>User Score: </strong> {this.state.score} < br />
+                </div>
+                <div className="resetForm">
+                    <button onClick={this.resetForm}>Next Question</button>
+                </div>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <div> <strong>User Score: </strong> {this.state.score} < br />
+                    <strong>Category: </strong> {category}<br />
+                    <strong>Question: </strong> {this.state.data.question} < br />
+                    <strong>Value: </strong> {this.state.data.value}< br />
+                </div>
+
+                <div className="jeopardyAnswer">
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <label htmlFor="answer">Your Answer: </label>
+                            <input
+                                type="text"
+                                name="jeopardyAnswer"
+                                value={this.state.formData.answer}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <button>Submit Answer</button>
+                    </form>
+                    <div>
+                        {this.state.formData.answer}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 export default Jeopardy;
